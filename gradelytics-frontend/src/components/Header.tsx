86026f7985f705ea14a84392436b05e4f60e8b66
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-01-28 21:46:04 +0530

    first commit

---
+++ gradelytics-frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div className='flex justify-center text-red-500 text-2xl'>
+      Header
+    </div>
+  )
+}
+
+export default Header
