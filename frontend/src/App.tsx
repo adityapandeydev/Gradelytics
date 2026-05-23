7abe5972473228da7716c5805dd906065ee29e3e
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-05-23 22:41:01 +0530

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline text-green-500">
         Will i finish this project this time?
+        Checking drive change
       </h1>
     </div>
   )
