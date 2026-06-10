19118616b9325a62ebfd788dc1e3da8b66740752
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-06-10 15:13:19 +0530

    june 10 commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline text-green-500">
         Will i finish this project this time?
         Checking drive change
+        Not happening, still very lazy.
       </h1>
     </div>
   )
